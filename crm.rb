662188decc7540948442014e3556f1de2e5612f8
LAURA LEIBOW <lauraleibow@gmail.com>
2017-01-24 14:55:30 -0500

    added post params and redirected to view all contacts

--- crm.rb
+++ crm.rb
@@ -4,12 +4,6 @@
 require_relative 'contact'
 require 'sinatra'
 
-#Fake data
-Contact.create('Mark', 'Zuckerberg', 'mark@facebook.com', 'CEO')
-Contact.create('Sergey', 'Brin', 'sergey@google.com', 'Co-Founder')
-Contact.create('Steve', 'Jobs', 'steve@apple.com', 'Visionary')
-
-
 get '/' do
   @crm_app_name = "Laura's CRM"
   erb :index
@@ -21,4 +15,9 @@ end
 
 get '/contacts/new' do
   erb :new_contact
+end
+
+post '/contacts' do
+  Contact.create(params[:first_name], params[:last_name], params[:email], params[:note])
+  redirect to('/contacts')
 end
