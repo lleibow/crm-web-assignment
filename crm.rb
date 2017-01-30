e3c49d1784a7f4eed146fab5642a4d3911553e51
LAURA LEIBOW <lauraleibow@gmail.com>
2017-01-29 20:32:57 -0500

    finihsed crm

--- crm.rb
+++ crm.rb
@@ -1,35 +1,82 @@
 # Implement the new web-based CRM here.
 # Do NOT copy the CRM class from the old crm assignment, as it won't work at all for the web-based version!
 # You'll have to implement it from scratch.
+
 require_relative 'contact'
 require 'sinatra'
 
-## Temporary fake data so that we always find contact with id 1.
-Contact.create('Betty', 'Maker', 'betty@bitmakerlabs.com', 'Developer')
-Contact.create('Johnny', 'Boner', 'johnyb@boner.nober', 'boner lover')
-
-
-get '/' do
-  erb :index
-  #erb :layout
+get "/" do
+  @num_contacts = Contact.all.count
+  erb :contacts
 end
 
-
-get '/new' do
+get "/contacts/new" do
   erb :new_contact
 end
 
-get '/about' do
-  erb :about
-end
-
-get '/:id' do
+get "/contacts/:id" do
   @contact = Contact.find(params[:id].to_i)
-  erb :show_contact
+  if @contact
+    erb :show_contact
+  else
+    raise Sinatra::NotFound
+  end
 end
 
+get "/contacts" do
+  @num_contacts = Contact.all.count
+  erb :contacts
+end
 
-post '/' do
-  Contact.create(params[:first_name], params[:last_name], params[:email], params[:note])
-  redirect to('/contacts')
+post "/contacts" do
+  contact = Contact.create(
+    first_name: params[:first_name],
+    last_name: params[:last_name],
+    email: params[:email],
+    note: params[:note]
+  )
+  redirect to ("/contacts")
+  # The Contact class method of creating instance variables and storing them
+  # Contact.create(params[:first_name], params[:last_name], params[:email], params[:note])
+  # redirect to("/")
+end
+
+get "/contacts/:id/edit" do
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
+    erb :edit_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+put "/contacts/:id" do
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
+    @contact = Contact.update(
+    @contact.id,
+    first_name: params[:first_name],
+    last_name: params[:last_name],
+    email: params[:email],
+    note: params[:note]
+    )
+
+    redirect to("/contacts")
+  else
+    raise Sintra::NotFound
+  end
+end
+
+delete "/contacts/:id" do
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
+    @contact.delete
+    redirect to("/contacts")
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+after do
+  ActiveRecord::Base.connection.close
 end
